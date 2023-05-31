99a8c5dd437087cb2a0765531549a251fe5e6380
wee-owl <kem-cv@yandex.ru>
2023-05-31 11:25:27 +0300

    fix: delete comments

--- src/js/daemonDamage.js
+++ src/js/daemonDamage.js
@@ -2,8 +2,3 @@ import AttackTypes from './attackTypes';
 
 export default class Daemon extends AttackTypes {
 }
-
-// import Magician from './magicianDamage';
-
-// export default class Daemon extends Magician {
-// }
